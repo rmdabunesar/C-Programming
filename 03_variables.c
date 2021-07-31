0b0ac641ac0e05e2617b116489d6b33da856a58d
rmdabunesar <abunesar13@gmail.com>
2021-07-31 21:54:11 +0600

    variables.c

---
+++ 03_variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int my_variable = 10;
+    my_variable =20;
+    printf("Variable value = %d\n", my_variable);
+
+    return 0;
+}
+
+
+
+// Variable is a container to contain Data( 0 or 1 ).
+//  Data_Type Variable_Name = Value( int my_variale = 10 )
