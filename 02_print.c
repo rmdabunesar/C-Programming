ddf1081e6d028d7d78647a6c41a0ec4ec9c5e364
rmdabunesar <abunesar13@gmail.com>
2021-07-31 21:15:40 +0600

    print.c

---
+++ 02_print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("This is my frist print in c programming.\n");
+    printf("This is Rinkon.\n");
+
+    return 0;
+}
+
+
+
+// "\n" is used to start new line.
