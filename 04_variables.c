00f5df2dd21e69a764acb107ca356ee70b7e7d88
rmdabunesar <abunesar13@gmail.com>
2021-08-01 17:06:15 +0600

    variables define and declaration.c

---
+++ 04_variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 60;
+    int b;
+    b = 10;
+    b = 20;
+
+
+    printf("%d + %d = %d\n", a, b, a+b);
+    printf("%d - %d = %d\n", a, b, a-b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d + %d = %d\n", a, b, a/b);
+
+    return 0;
+}
+
+
+// Variable Define(one time) and Declaration.
