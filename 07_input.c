ce846954f0dd722dbc5c843bc73ca0586d458779
rmdabunesar <abunesar13@gmail.com>
2021-08-01 18:21:28 +0600

    input.c

---
+++ 07_input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+    float c,d;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("print a b = %d %d\n", a, b);
+
+    scanf("%f %f", &c, &d);
+    printf("print c d = %f %f\n", c, d);
+
+    return 0;
+}
