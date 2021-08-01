4b58b742cb0c88bc259c77dc30ffb737b915c211
rmdabunesar <abunesar13@gmail.com>
2021-08-01 18:07:15 +0600

    constants.c

---
+++ 06_constants.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    const float PI = 3.1416;
+    printf("%.4f\n", PI);
+
+    return 0;
+}
+
+
+
+/**
+C Reserved Keywords
+-----------------------------------------
+auto	    else	long	    switch
+break	    enum	register	typedef
+case	    extern	return	    union
+char	    float	short	    unsigned
+const	    for	    signed	    void
+continue	goto	sizeof	    volatile
+default	    if	    static  	while
+do	        int	    struct	    _Packed
+double
+**/
