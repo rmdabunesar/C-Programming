88390ceba81b0e87e1584a6690f41e396813b447
rmdabunesar <abunesar13@gmail.com>
2021-08-01 22:58:19 +0600

    character data type in c

---
+++ 11_char-data.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    // char - %c - 1 bytes
+    char a = 'A';
+    char d = a + 3;
+    printf("char - %c\n", a);
+    printf("ASCII code for 'A' - %d\n", a);
+    printf("ASCII code for 'D' - %c\n", d);
+
+    return 0;
+}
