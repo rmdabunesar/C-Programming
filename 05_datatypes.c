ccfd133cfe9456ff00a49ffebe18004681a38a68
rmdabunesar <abunesar13@gmail.com>
2021-08-01 17:24:32 +0600

    datatypes.c

---
+++ 05_datatypes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1 =45;
+    float num2 = 45.45;
+    char lett = 'T';
+
+    printf("%d\n", num1);
+    printf("%f\n", num2);
+    printf("%c\n", lett);
+
+    return 0;
+}
