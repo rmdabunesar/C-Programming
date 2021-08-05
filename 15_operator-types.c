ad3629738583e3083974127d3440d923f6a68710
rmdabunesar <abunesar13@gmail.com>
2021-08-05 17:53:18 +0600

    oparator types in c

---
+++ 15_operator-types.c
@@ -0,0 +1,31 @@
+#include <stdbool.h>
+
+int main(void) {
+
+
+
+    return 0;
+}
+
+
+
+/* 
+# what is operand and operator?
+    a + b => here '+' is operator, 'a' and 'b' is operand.
+
+# there are mainly three types of operator available -
+    > unary
+    > bainary
+    > ternary
+
+    unary operators: only have one operand. ex: a++, --b
+    binary operators: have two operand. ex: a + b, a - b
+        there are different type of binary operator are available -
+        > arithematic (+, -, *, /, %)
+        > relational (==, >, <, !=, >=, <=)
+        > logical (&&, ||, !)
+        > assignment (=, +=, -=, *=, /=, %=)
+        > bitwise (&, |, ^, ~, <<, >>)
+    ternary oparators: have three operand. ex: conditional operator
+
+*/
