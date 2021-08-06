274bc06d73f4e371ee5b42525c694fcf8feb2a30
rmdabunesar <abunesar13@gmail.com>
2021-08-06 10:23:47 +0600

    what is condition in c

---
+++ 23_condition.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    
+
+    
+    return 0;
+}
+
+
+/*
+# what is condition?
+    -conditions are way to implement a logical situation. in programming, sometimes we have different situation and based on one situation we have to execute some sort of code rather than other situation.
+
+# ex: if a user logged in our system, we want to show their name. otherwise, we want to show login button.
+
+# condition statement in c programming -
+    1. if condition
+    2. else condition
+    3. else if condition
+*/
