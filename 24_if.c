130eb337577df6b968b11e6a33027cfdd7822f0a
rmdabunesar <abunesar13@gmail.com>
2021-08-06 10:36:17 +0600

    if statement in c

---
+++ 24_if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    bool condition = true;
+
+    if (condition)
+    {
+        printf("I can execute as the condition is true.\n");
+    }
+    
+
+    return 0;
+}
