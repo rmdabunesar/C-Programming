ff83394ed98052033accff6e10cd80df63110440
rmdabunesar <abunesar13@gmail.com>
2021-08-07 01:05:54 +0600

    loops in c

---
+++ 32_loops.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+
+    return 0;
+}
+
+
+
+/*
+# what is loops?
+    -the loop is a statement that repeats a given task for a given amount of time.
+    ex. find the even number between 1~100.
+
+# why do we use a loop?
+    -in programming, we have always the same relative task. to reduce repeated code, we use a loop.
+    ex. we have a student database and we want to sent notifications to each of them. manually it is impossible to send notification one by one.
+
+# type of loops in c programming
+    1. for loop (follow step)
+    2. while loop (follow condition)
+    2. do while loop (follow condition but execute at least one time)
+*/
