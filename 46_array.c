dc171a03466b4f73c8d901d1ecc13720f2b674d8
rmdabunesar <abunesar13@gmail.com>
2021-08-17 10:39:34 +0600

    array in c

---
+++ 46_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int my_array[10];
+
+    my_array[0] = 'n';
+    my_array[1] = 'e';
+    my_array[2] = 's';
+    my_array[3] = 'a';
+    my_array[4] = 'r';
+
+    printf("%c\n", my_array[0]);
+    printf("%c\n", my_array[1]);
+    printf("%c\n", my_array[2]);
+    printf("%c\n", my_array[3]);
+    printf("%c\n", my_array[4]);
+
+    return 0;
+}
